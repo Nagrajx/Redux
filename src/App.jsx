d7a7a7d1438bc0d35354f10a138d78ef3f609e63
Nagrajx <satishnagraj54@gmail.com>
2026-06-13 22:49:56 +0530

    Initizlised Project with ReduxToolKit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes ,Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+         <div className='h-screen  text-white w-full bg-gray-950'>
+           
+  </div>
+  )
+}
+
+export default App
